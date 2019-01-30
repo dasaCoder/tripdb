9d9354bdc46bd26acbcfbc54bafc636c72aa06e7
Dilusha100 <dilushadasanayaka@gmail.com>
2019-01-30 14:33:26 +0530

    app build

--- App.js
+++ App.js
@@ -1,11 +1,33 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, TextInput } from 'react-native';
 
 export default class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {text: 'cghss'};
+  }
+
   render() {
+    let pic = {
+      uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
+    };
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+      <View style={{flex: 1}}>
+        <View style={{flex: 1, backgroundColor: 'powderblue'}} >
+        <Text>lki{this.state.text}</Text>
+        </View>
+       
+        <View style={{flex: 1, backgroundColor: 'steelblue'}} >
+        
+            <TextInput style={{height: 40}}
+                placeholder="Type here to translate!"
+                onChangeText= {(text)=> this.state =text}
+            >
+            </TextInput>
+
+          
+        </View>
       </View>
     );
   }
